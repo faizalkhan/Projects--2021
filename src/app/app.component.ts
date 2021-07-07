e684de49f52863fcccc2bf9c87effceb2f4f8a60
faizalkhan <faizalkhan.s@gmail.com>
2021-07-07 12:02:45 +0530

    Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angularlifecycle';
+  names = 'Angular life cycle';
+  show = false;
+  toggle()
+  {
+   this.show = !this.show;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule,  } from '@angular/core';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { FormsModule } from '@angular/forms';
+import { UserformComponent } from './userform/userform.component';
+import { FieldComponent } from './field/field.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    UserformComponent,
+    FieldComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
